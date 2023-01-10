31c2893fa76e729d9463a762661c25f8d3dae0df
activehubb <activehubb@gmail.com>
2023-01-10 00:46:32 +0000

    update

---
+++ config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	google: {
+		clientID:
+			'914851578993-25h7qf53h1bqaf2frmdavidk6dbq8c17.apps.googleusercontent.com',
+		clientSecret: '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',
+		callbackURL: '/auth/google/redirect',
+	},
+	mongoDB_URI:
+		'mongodb+srv://postol:~Sin2+Cos2=1~@postolcluster.xvtqz1m.mongodb.net/?retryWrites=true&w=majority',
+	session: {
+		key: "cookie_session_1!1!1!"
+	}
+};
